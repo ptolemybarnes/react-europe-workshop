bc3b8c6c148c67dc1d044e317d8321fce47bb205
Ptolemy Barnes <Ptolemy.Barnes@thetrainline.com>
2017-05-16 12:58:55 +0100

    Solution

--- src/withMinLengthValidation.js
+++ src/withMinLengthValidation.js
@@ -1,1 +1,22 @@
 import React from 'react';
+
+const renderWarning = (min) => (
+  <span>
+    { 'Minimum ' }
+    { min }
+    { ' required.' }
+  </span>
+)
+
+export default (Component, minimumLength) => (
+  props => {
+    const { value } = props;
+    const showWarning = ('' + value).length < minimumLength;
+    return (
+      <div>
+        <Component { ...props } />
+        { showWarning && renderWarning(minimumLength) }
+      </div>
+    )
+  }
+)
